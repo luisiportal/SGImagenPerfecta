7be6546f3bc55b72330ad403eaeb73a480ebb793
luisiportal <luisiportal@gmail.com>
2024-03-30 22:54:25 -0400

    actualize las v entorno

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://192.168.1.1:4000/",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "http://192.168.1.1:4000/",
   withCredentials: true,
 });
 
